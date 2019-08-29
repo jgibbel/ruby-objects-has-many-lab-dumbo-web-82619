94f395c8341ab4b78981c22e29ed22d0e19c01ed
James Gibbel <jamesgibbel@gmail.com>
2019-08-29 14:15:22 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,23 +1,9 @@
-class Artist
+class Author
   attr_accessor :name, :songs
   
- 
   def initialize(name)
     @name = name
-    @songs = []
   end
   
-  def add_song(song)
-    @songs << song 
-    song.artist= self 
-  end 
-  
-  def add_song_by_name(title)
-    Song.new(title).artist=(self)
-  end
-  
-  def self.song_count()
-    Song.number
-  end 
   
 end
